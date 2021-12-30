276075f95347e92392af6a6c41a50af210b6b83b
goppy <tacumin.goppy.06@gmail.com>
2021-12-30 16:46:23 +0900

    change

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -39,9 +39,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  axios: {
   }
 }
